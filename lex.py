1fd9623f1c67d1acb518839bb74b1cab32aa015b
Alexandra Wong <lex115wong@gmail.com>
2022-11-23 17:49:19 -0800

    Format crab ascii art

---
+++ lex.py
@@ -0,0 +1,96 @@
+from art import *
+
+print(r"""\
+                                                                                                                                                      
+                                             ``                                                                                                       
+                                             i:                                                                                                       
+                                            ,*.                                                                                                       
+                                           `;:                                                                                                        
+                                           ,:,                                                                                                        
+                                          `::`                                                             ``                                         
+                                          ,,:`                                                             .+`                                        
+                                         .,,,                                                              `i:                                        
+                                        ,+,,,                                                               ::`                                       
+                                       .ii,,.                                                               ::.                                       
+                                      `;;:,:`                                                               ::.                                       
+                                     `:;:.,:`                                                               ,:.`                                      
+                                     ,;::,,,                                                                ,:..                                      
+                                    `;:::;:,                                                                ,:..                                      
+                                    ,;,,:;;.                                                                ,;,,`                                     
+                                   `;:,:,:i,                                                                ,i,,`                                     
+                                   ii,,,,:i:                                                                ,**:.                                     
+                                  ,+;,,:::i.                                                                ,ii;,                                     
+                                  i+;,,;i;i`                                                                ,;;::.                                    
+                                 `*+;,:***i`                                                                :i;:,;`                                   
+                                 ,++;,;+++;                                                                `i*i:,:,                                   
+                                 :++i;*+++:                                                                `**i:,.:`                                  
+                                 ;+#**++#+,                                                                .*ii:,.,,                                  
+                                 i+#+*++##.                 .,                                             .*ii:,..:                                  
+                                 i#++++##+:..              ;@W*                                            ,***:,.,;`                                 
+                                 ;##+++####z*             `WW#x`                :n+                        :+#+:,,;i.                                 
+                                 :###++##*#zni            ,@#WW.               `xMW:                       ;##+;,:i*,                                 
+                                 .###++#+i##zn.           .@#WW+;;;ii:,`  ``..`+WWW*                       iz#+;:;**,                                 
+                                  i###++**#zzn*         `.:W#WWWn#nxMMx#i+znxxnM###*`                    .i+z#+i;***,                                 
+                                  .####+*+##znn`     `:*zxM@@WWW@WWMMMMxxMWWWWMW@#@#i:,`                `#xzz#+*i*+*.                                 
+                                   i#zz#+zz#znn:   ,#MWWM@@##@@W@@MxnnxMMMMMW@@@@#@Wx#*i,``             +xx#z#++*+**`                                 
+                                   i+###zz##+#xi `+MWWWWMW@@###@@MnnnnznnnxxW@@W@@@@WMx#**i;:.         ,xxxzz##++++i                                  
+                                   ,####z#i:;i#; :MWWWWWWMWW@##@@MxnxxxxnnnxM@#@@MM@@MxMxxxnnz*.       #xMxzzz#++++,                                  
+                                    :####i:::;*zi*MWWW@WWWWWW@@WWWWWWWWWMxxWWMWW@xxMMMnnnznxMMMni`    .MxMxnn#####*`                                  
+                                 ``,.inn+;;;;;*x*#MMWW@@WWWWW@@WWWWW@W@@WW@@WWWWWWWMMxnxnnnxMWWWWz.   :MxxMxnz####:                                   
+                             `,i+++xnnnz#;i;:i#ninMxxMMMMxxxM@WMnWWWWMMW@@@@WW@@WMMxxxnzznnxMWWWWWM:  :znxxxnzz##*,                                   
+                           `:+nxxxMMMMMz#i;;ii+nixMxxxxMMMxxMMxnxWW@@@W@@@@WMMx@Mxxnnn###zxMMMMWWWWx,#*++znnnzz#**`                                   
+                          .*nxxxxMMMMMM#++**#+#x+xMxxxxxxxxxxnnMMxxxW@MWWMMWMxxxMxxn#++++#znxMMWWW@M#x***+znnz#++.                                    
+                         .+xxxxxxMMMMMMx+#zzzznnzxMMnxxxxxxxxnxMxxxxnMWMxxMxxMxxxnnn###zzzznnxxMWW@@nn+**+++nnz#.                                     
+                        .#xxxxxn;zxMMMMMnzzzzzznnxMMxxxxxxxxxxMMxMxxxnMxxxxxxnxxxxxxxnnnnnnnnxxxMW@@nn+*i*iizz*`                                      
+                       `;nxxxxn*.,zMMMMMx#zzzzzxxMWMxxxxxxxxxMMMxMxxxnMxxxMMxxxxxxxnxnnnnnnnxxxxxMWWzn#*;;i*z,                                        
+                      .iznnnnnz. `:xMMMMM#zznnnnxMWWxxxxxxxxMMMMMMxxxzMxMMMMMxxxxxxxnnnnnnnxxxxxxxMxzzz+*;;+#.       ``.`                             
+                     .+xnxnnnn;` `:nMMMMMz#znnnnxMWWMxxxnxxxxMMMMMxxzxMnMMMxxxxnxxxxnnnnnxxxxxxxxMMz##zzz#++:      .i#zz#i:`                          
+                    ,#nxxxnnn*.  `;nxMMMMx##nnnnnMWWMxxxxnnxnxxxMxxxzxMznxxxxxxnxxxnnnnnnnxxxxxxxMx##+##++#;     `;znnnnnzzzi`                        
+                   ,#nnnnnnn#,`  .#nnMMMMMz#znnnnxWWMMxnnnnnnnnxxxxz#zxnznxxxxnnnxnnnnnnnnxxxxxxMM#######++`    .+nznnzzzz#zz*`                       
+                  ,#nnnnnnnni`  `:zxnxMMMMM##nnnzzMWWMxxnnnxnnnnnzzn+*nzz#znzzznnnnnnnznnnxxxxxxMn*i#####+;`.,.:znnnnnnzzz#z#*i`                      
+                 .+nnnnnnnzi.`  `;nxxxMMMMMx#znnzznWWMMxxnnxnnnz#znxnnMxzz#+zznnnnzzzznnxxxxxxxxM#iii*+#+#*znnznnnznnzzzz#+z#*i;`                     
+                `*nnnnzzzzi.`   `inxxxxMMMMMn#znzzzMWWMxxnznnnnnnz#zxxMxz##znnnznnzzznnxxxxxxxxMx*;i*ii++nxxxnnnnnnnnzzz+*.*z+ii,                     
+                ,znnzzzz+;.`    `inxxxznMMMMMn##+###zMWMxnzznnnnnz#+nxxx##znnnznznzzznnxxxxxxMMM#*iiiii*nxxxnnnnnnzzzzz#zn:*z#*ii`                    
+               `;nnzzzz*,.`   ```innnn+:nMMMMMz#+##z**xWWMxnnnnnzz#++nnn++znnzzzzn##znnnxxxxxMMn+*+*iiizxxxnnnnnnzzzzz#zxxnnz##i+;                    
+               `inzzz+;..``.:;;;;inxnn;`;xMMMMMnz+##+i*#nMMMMnzzz#++#nnn#++zzzzz#zz#zznnxxMMMMn#*##+**zxxxnnnnnzzzzzz##xxxxnz##*++;`                  
+               `*zz+i,..``:#nxxxxxxxxz;..ixMMMMMx++++i;;i*+zxnzzz#+#++#z##+#zzzzz#znxxxxxxMMxz##++**+zxxxnnnnnzzzzzz##xxxnnzz#+*++##i.                
+               .*++:.....,*nnxxxxxxxxxxz+*#xMMMMWn##**iii;i+#zz####++**+++#+#z###+#nnnnxnnnnz#*++++#nxxxnnnnnzzzzzzzzxxxnn#:;#+**+#zz#,               
+               .*+*,..:*#znxnxxxxxxxxxxxxMxMMMMMMWWxz+*i;i**;+nnzz##+*####+++++#+#+z+++##+*###**++#nxxxxnnnzzzzzz#zzznxnzz#;`#z##+#z#zz+,             
+               .*+*::+nnzzznzznxxxxxxxxxxxxxxMMMMMMWxx#**+#*;+nzz##++##**+#++##+##++*+***ii*++**zzxxxxxxnnzzz####znzzznzzzz*`:z####zzzzzzi.           
+              `.**i*nnn#zz#+zznnnnnnnxnnxxxxxxMMMWMMMx####*;*#nnzz###z#+*i+++++###*+i#+*iii*++*+nxxxxxxnz####znnxxxznz##zzz; `*z#####znzzz+,          
+              `,*++znn##z+++###zzznzzznnnnxxxxxMMMxxxxx#+ii+#zzzz##+###++**+++####*+i*#+ii+#znnxxxxxxxzznnxxxxxxxxxnzn#.#z#.  .+z#####zzzzzz;`        
+              `,+nzzz+*#*;i**##zz##++++##zznnnnxxnnznxxxxn+++++###+++####+*+**++++*i+**++++nxxnxnnnnnxxxxxxxxxxxxxnnznx*+#;` ``.+#####zzzzzz#i`       
+             `.,+zzz#i*#:::::i#zzzzz#+***+++##zxnz###zzzn#****++++++####+****ii*****i+*ii*#nnnnnznnnxxxxxxxxxxxxnnnnnnn#+;` ````,+######zzzzz#;`      
+             `.:+zz#++#;,,,,,,,:i#zzzzz##+##z#nn##i;;*#zz+**+++++++#+++#z#*i***iiiiiii+#znnzzzzzznnnxxxxxxxxxxxnnnzznnn+;` ```` `,#######zzz###.      
+            `.,#znnz#+*,,,,,..,,,,:;*######++*i;;::::;i*++**+++++**+*++++**i;;i+++****iiinnzzzz#znnxxxxxxxxxxnnnnznnnzz:````` ````,+####+#####z;      
+            `.+nnnnz#+:.............,,,::;:,,,,,,,,::::::::;;;;;iiiiii******ii;i+++++++***###zzz#znxxxxxnnnnnnnnnnnzzz+,.``````````.+#+++,;+#z#i`     
+           ``izznznz#*....````.`````.....,........,,,,,,,,,,,,,,,,,,:::;;iiiiiiii*ii*****+#zznnnz+*##zzzzzznnnnnn#z#zz;...``````````,+##+,.,:*+*,     
+           `:znzzzzz+,...```````````````.........................,,,,,,,,,,,,,,,,,:::::::::;;;;;:::,,:;*+++*i;inn####+,...``````````.,*#*,...:*i:`    
+           .+zzzz###,.```````````````````````````````.............................,,,,,,,,,,,,,,,,,,..,,,,,,,:znnz#++:,...``````````..i*i,....ii;`    
+           :zzzzz#+,``````````````````````````````````````````````````````.................................,,*nnnzz#*,...```````````..**i,....;**`    
+          `+zzzz#+:```````````                  ```````````````````````````````````..........................#nnnzz+,...````````````..**i,,...:#+.    
+          .#zzz#+:.``````                                         `````````````````````````````````````````.,nznnzz;.````````````````.**:....,:##.    
+         `.+nz#+:..``````                                                ```````````````````````````````````*nzzzzz,`````````````````:**....`.,;;`    
+          `+zz#;.`````                                                              `   ```````````````````.#zzzzz+``  ````` ````````;+i.````````     
+          `i#+*i:````                                                                             `````````:zzzzzz;             `````+#,````          
+           `;*iii:.                                                                                ````````*zzzz#+`              ```.#z.``            
+           `.:iii;;.`                                                                               ``````.#zzz##,                ``:#;`              
+            `.,:ii;;,`                                                                              ``````,zzz##*`                  ,:`               
+             `..,;i*i;.                                                                              `````:###+*.                                     
+              ``..,:;iii.                                                                            `````;+##*,                                      
+                 ````..,:.                                                                           ```.:+*+i:`                                      
+                                                                                                   `````;**ii.`                                       
+                                                                                                   `` `;iiii.`                                        
+                                                                                                    ``;ii*;``                                         
+                                                                                                    .;*ii,`                                           
+                                                                                                  `.+**i.`                                            
+                                                                                                 `:*++,`                                              
+                                                                                               `:*+*:.                                                
+                                                                                              `ii:,`                                                  
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+
+""")
+tprint("True Final Boss")
