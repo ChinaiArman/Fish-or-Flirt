643076ee7fd8ffcaffc7f06a592767a323cced3a
Alexandra Wong <lex115wong@gmail.com>
2022-11-24 00:38:03 -0800

    Wrote encounter dialogues

--- lex.py
+++ lex.py
@@ -94,3 +94,71 @@ print(r"""\
 
 """)
 tprint("True Final Boss")
+
+# each random counter (land and sea) crabs fisherman fish mermaids
+# each non encounter (land and sea)
+# each special encounter leviathan
+
+greeting = "Hello. We all know why you're here. \n" \
+           "You wish to fish. \n" \
+           "You want to warp a carp, \n" \
+           "getta a betta, net a flounder ten-pounder, snag a daring red haring– \n" \
+           "OK. \n" \
+           "I'll stop embarrassing you. \n" \
+           "Here. \n" \
+           "Take a fishing rod, \n" \
+           "and go wild kid."
+
+random_encounter_crab = [
+    "You were thoroughly enjoying the feeling of sand beneath your toes, until a crab decides your toes were looking at it the wrong way. The crab challenges you to a duel.",
+    "You see a crab. You are filled with hatred. The crab is also filled with hatred. You are hatred itself. The crab one-ups you and decides that it is despisal. You and the crab lock eyes and the first one to blink loses.",
+    "Crab. What more should I say? Crab.",
+    "You step forward, the crab steps forward. You step left, and it, left. You try to eurostep that fool, but you are no match for the agility of the magnificent crab, and gets your ankles broken"
+]
+
+random_encounter_fisherman = [
+    "You see another person, wearing a straw hat, holding a rod and a bucket in each hand. You both startle and point and each other in excitement. Neither of you say a word, but the intent is clear. FISH.",
+    "The only living being a fisherman hates more than a crab, is another fisherman. You see another fisherman.",
+    "You see a person, with a fish. What a nice fish, you think. They see a person with a fish. What a nice fish, they think.",
+    "You see a person with a rod, and a bucket. There is no fish in their bucket. You are filled with overwhelming pity for them. You are filled with overwhelming self-superiority over them. This is the start of your villain arc."
+]
+
+random_encounter_fish = [
+    "FSIH FISH FISHS FSIHDSF FSIFH FSIHF FISHF FISHF FISH. There's a fish.",
+    "It's your lucky day! You've found a strange looking being with 21 limbs, "
+    "oil and tar dripping from its skin and sliding onto the sea. It looks at you with beady, soulless eyes, "
+    "and without knowing the reason, you are overcome with the desire to look away. But you must keep your eyes on it, "
+    "after all, IT'S SUCH A COOL FISH!!",
+    "You found a fish. Good job buddy.",
+    "It looks like a fish, it smells like a fish, it sounds like a fish, it feels like a fish, it tastes like a fish. Gasp! I think you've found a fish!"
+]
+
+random_encounter_mermaid = [
+    "Me-me-mermaid... Pretty.",
+    "You see a person in the water! They may be drowning! "
+    "You try to rush over towards them before you are met with many many many many many many many many many many many "
+    "many many many many many many many many many many many many many many many many many many many many many many many "
+    "many many teeth. Whew! It was only a mermaid. Mermaids can't drown silly. Don't worry about it.",
+    "There's a mermaid. It's a mermaid.",
+    "'Wow!' You think, 'What a big fish!' A mermaid hits you for calling them derogatory terms."
+]
+
+no_encounter_land = [
+    "I don't like sand. ( ｡ •̀ ᴖ •́ ｡) It's coarse and rough and irritating and it gets everywhere.",
+    "WOW! CONGRATS! YOU FOUND \nseaweed... You found wet soggy seaweed... congrats",
+    "What kind of fisherman stays landlocked! Go to the sea, you'll find nothing on this barren land.",
+    "The 'racle hast possess'd me, I f'resee a prophecy... \nGasp! the young gudgeon'rman finds nothing h're"
+]
+
+no_encounter_sea = [
+    "You set forward on your little boat in search of bigger fish to fry, but to your demise, you catch not even a single fly",
+    "You thought you saw a fish in this direction. Turns out you thought wrong.",
+    "You throw out your fishing rod. It returns you a chewed through boot with a hole in the toe.",
+    "You came, you saw, you left empty handed. Ain't no fish here pal!"
+]
+
+encounter_leviathan = "Abandon hope all ye who enters here. \n" \
+                      "For here lies the lair of Leviathan, ruler of the deep and vicious seas. \n" \
+                      "If you unlucky sailors find yourselves in its maw, despair.\n" \
+                      "<You have reached the final boss, the Leviathan! Are your stats high enough to fish or " \
+                      "capture the heart of this fearsome foe? Take up your rod and find out!>"
