1ba0179c75062076a6986065ff7b781c63115c30
ChinaiArman <chinaiarman@gmail.com>
2022-12-03 02:43:44 -0800

    Update character_has_leveled unit tests

--- Unit Tests/test_character_has_leveled.py
+++ Unit Tests/test_character_has_leveled.py
@@ -4,9 +4,11 @@ from game import character_has_leveled
 
 class TestCharacterHasLeveled(TestCase):
     def test_xp_and_rod_equal(self):
-        character_level_stats_equal = {'xp': 1, 'rod level': 1}
-        self.assertEqual(character_has_leveled(character_level_stats_equal), False)
+        expected = False
+        actual = character_has_leveled({'xp': 1, 'rod level': 1})
+        self.assertEqual(expected, actual)
 
     def test_xp_greater(self):
-        character_stats_xp_greater = {'xp': 1, 'rod level': 0}
-        self.assertEqual(character_has_leveled(character_stats_xp_greater), True)
+        expected = True
+        actual = character_has_leveled({'xp': 1, 'rod level': 0})
+        self.assertEqual(expected, actual)
