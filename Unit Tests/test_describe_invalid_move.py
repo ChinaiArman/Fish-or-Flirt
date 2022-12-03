42fd206b46f568dc37db6c21a9e7e2496e72f323
ChinaiArman <chinaiarman@gmail.com>
2022-12-03 02:48:14 -0800

    Update describe_invalid_move unit tests

--- Unit Tests/test_describe_invalid_move.py
+++ Unit Tests/test_describe_invalid_move.py
@@ -13,31 +13,34 @@ class TestDescribeInvalidMove(TestCase):
         user_position = {'x-coordinate': 0, 'y-coordinate': 0, 'rod level': 0}
         simple_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): WATER_TILE, (1, 1): LAND_TILE}
         move = "fishing"
-        expected_output = "I'll be surprised if you can reel in any fish here, seeing as there are no fish here."
-        self.assertEqual(describe_invalid_move(simple_board, user_position, move), expected_output)
+        expected = "I'll be surprised if you can reel in any fish here, seeing as there are no fish here."
+        actual = describe_invalid_move(simple_board, user_position, move)
+        self.assertEqual(actual, expected)
 
     @patch('game.randint', side_effect=[1])
     def test_invalid_water(self, _):
         user_position = {'x-coordinate': 1, 'y-coordinate': 0, 'rod level': 0}
         simple_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): WATER_TILE, (1, 1): LAND_TILE}
         move = "north"
-        expected_output = "You look into the void.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThe" \
-                          " void looks back.\nYou should turn back"
-        self.assertEqual(describe_invalid_move(simple_board, user_position, move), expected_output)
+        expected = "You look into the void.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThe void " \
+                   "looks back.\nYou should turn back"
+        actual = describe_invalid_move(simple_board, user_position, move)
+        self.assertEqual(expected, actual)
 
     @patch('game.randint', side_effect=[2])
     def test_invalid_land(self, _):
         user_position = {'x-coordinate': 0, 'y-coordinate': 0, 'rod level': 0}
         simple_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): WATER_TILE, (1, 1): LAND_TILE}
         move = "east"
-        expected_output = "The crabs look at you with disgust. What are you, stupid? You can't go there! Go " \
-                          "somewhere else!"
-        self.assertEqual(describe_invalid_move(simple_board, user_position, move), expected_output)
+        expected = "The crabs look at you with disgust. What are you, stupid? You can't go there! Go somewhere else!"
+        actual = describe_invalid_move(simple_board, user_position, move)
+        self.assertEqual(expected, actual)
 
     @patch('game.randint', side_effect=[1])
     def test_invalid_island(self, _):
         user_position = {'x-coordinate': 1, 'y-coordinate': 1, 'rod level': 0}
         simple_board = {(0, 0): ISLAND_TILE, (0, 1): LAND_TILE, (1, 0): WATER_TILE, (1, 1): ISLAND_TILE}
         move = "east"
-        expected_output = "Let's review our NUMBERS. 1 for NORTH. 2 for SOUTH. 3 for EAST. 4 for SOUTH. Try again."
-        self.assertEqual(describe_invalid_move(simple_board, user_position, move), expected_output)
+        expected = "Let's review our NUMBERS. 1 for NORTH. 2 for SOUTH. 3 for EAST. 4 for SOUTH. Try again."
+        actual = describe_invalid_move(simple_board, user_position, move)
+        self.assertEqual(expected, actual)
