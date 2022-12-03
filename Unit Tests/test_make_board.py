0db8e7078c9a8d99e189af980f8ec7fe1277d5c0
ChinaiArman <chinaiarman@gmail.com>
2022-12-03 03:37:55 -0800

    Create unit tests for make_board

--- Unit Tests/test_make_board.py
+++ Unit Tests/test_make_board.py
@@ -1,6 +1,18 @@
 from unittest import TestCase
 
+from game import LAND_TILE, WATER_TILE
+from game import make_board
+
 
 class TestMakeBoard(TestCase):
-    def test_make_board(self):
-        self.fail()
+    def test_make_board_rows_columns_10(self):
+        test_board = make_board(10, 10)
+        expected = 100, LAND_TILE
+        actual = len(test_board), test_board[9, 0]
+        self.assertEqual(expected, actual)
+
+    def test_make_board_rows_columns_16(self):
+        test_board = make_board(16, 16)
+        expected = 100, WATER_TILE
+        actual = len(test_board), test_board[15, 0]
+        self.assertEqual(expected, actual)
