09dbb32bd3450f802d7041d9a80bba148cd80bc9
ChinaiArman <chinaiarman@gmail.com>
2022-12-03 02:20:36 -0800

    Move unit tests back into folder

--- Unit Tests/test_describe_invalid_move.py
+++ Unit Tests/test_describe_invalid_move.py
@@ -1,6 +1,43 @@
 from unittest import TestCase
+from unittest.mock import patch
+
+from game import describe_invalid_move
+from game import WATER_TILE
+from game import LAND_TILE
+from game import ISLAND_TILE
 
 
 class TestDescribeInvalidMove(TestCase):
-    def test_describe_invalid_move(self):
-        self.fail()
+    @patch('game.randint', side_effect=[2])
+    def test_fishing(self, mock_randint):
+        user_position = {'x-coordinate': 0, 'y-coordinate': 0, 'rod level': 0}
+        simple_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): WATER_TILE, (1, 1): LAND_TILE}
+        move = "fishing"
+        expected_output = "I'll be surprised if you can reel in any fish here, seeing as there are no fish here."
+        self.assertEqual(describe_invalid_move(simple_board, user_position, move), expected_output)
+
+    @patch('game.randint', side_effect=[1])
+    def test_invalid_water(self, mock_randint):
+        user_position = {'x-coordinate': 1, 'y-coordinate': 0, 'rod level': 0}
+        simple_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): WATER_TILE, (1, 1): LAND_TILE}
+        move = "north"
+        expected_output = "You look into the void.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThe" \
+                          " void looks back.\nYou should turn back"
+        self.assertEqual(describe_invalid_move(simple_board, user_position, move), expected_output)
+
+    @patch('game.randint', side_effect=[2])
+    def test_invalid_land(self, mock_randint):
+        user_position = {'x-coordinate': 0, 'y-coordinate': 0, 'rod level': 0}
+        simple_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): WATER_TILE, (1, 1): LAND_TILE}
+        move = "east"
+        expected_output = "The crabs look at you with disgust. What are you, stupid? You can't go there! Go " \
+                          "somewhere else!"
+        self.assertEqual(describe_invalid_move(simple_board, user_position, move), expected_output)
+
+    @patch('game.randint', side_effect=[1])
+    def test_invalid_island(self, mock_randint):
+        user_position = {'x-coordinate': 0, 'y-coordinate': 0, 'rod level': 0}
+        simple_board = {(0, 0): ISLAND_TILE, (0, 1): LAND_TILE, (1, 0): WATER_TILE, (1, 1): LAND_TILE}
+        move = "east"
+        expected_output = "Let's review our NUMBERS. 1 for NORTH. 2 for SOUTH. 3 for EAST. 4 for SOUTH. Try again."
+        self.assertEqual(describe_invalid_move(simple_board, user_position, move), expected_output)
--- Unit Tests/test_loading.py
+++ Unit Tests/test_loading.py
@@ -1,6 +1,15 @@
+import io
+import unittest.mock
+import time
 from unittest import TestCase
+from dialogue import loading
 
 
 class TestLoading(TestCase):
-    def test_loading(self):
-        self.fail()
+    @unittest.mock.patch('time.sleep', return_value=None)
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_one_instance(self, patched_sleep, mock_output):
+        loading(1)
+        program_output = mock_output.getValue()
+        expected_output = "...\n\n"
+        self.assertEqual(program_output, expected_output)
