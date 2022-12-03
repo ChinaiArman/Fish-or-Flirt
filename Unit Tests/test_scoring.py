d0e8b69bae0940dc83fa962c1cbc5f3c5ef7d012
ChinaiArman <chinaiarman@gmail.com>
2022-12-03 03:48:05 -0800

    Create unit tests for scoring

--- Unit Tests/test_scoring.py
+++ Unit Tests/test_scoring.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
 
+from game import scoring
+
 
 class TestScoring(TestCase):
-    def test_scoring(self):
-        self.fail()
+    def test_scored_item(self):
+        expected = ('Shark', 3, 60)
+        actual = scoring('Shark', 3)
+        self.assertEqual(expected, actual)
+
+    def test_item_not_scored(self):
+        expected = ('banana', 5, 0)
+        actual = scoring('banana', 5)
+        self.assertEqual(expected, actual)
