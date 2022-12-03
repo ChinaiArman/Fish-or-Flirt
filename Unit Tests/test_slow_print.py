de21d87826f694293b520751f48d59d224ee96a7
ChinaiArman <chinaiarman@gmail.com>
2022-12-03 04:05:21 -0800

    Create unit tests for validate move

--- Unit Tests/test_slow_print.py
+++ Unit Tests/test_slow_print.py
@@ -1,15 +1,13 @@
 import io
 import unittest.mock
 from unittest import TestCase
-
 from dialogue import slow_print
 
 
 class TestSlowPrint(TestCase):
-    @unittest.mock.patch('dialogue.sleep', return_value=None)
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_string(self, patched_sleep, mock_output):
+    def test_print_string(self, mock_stdout):
         slow_print("Hello World")
-        printed_statement = mock_output.getValue()
+        printed_statement = mock_stdout.getValue()
         expected = "Hello World.\n"
         self.assertEqual(printed_statement, expected)
--- Unit Tests/test_validate_move.py
+++ Unit Tests/test_validate_move.py
@@ -1,6 +1,78 @@
 from unittest import TestCase
 
+from game import LAND_TILE, WATER_TILE
+from game import validate_move
+
 
 class TestValidateMove(TestCase):
-    def test_validate_move(self):
-        self.fail()
+    def test_direction_false(self):
+        test_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): LAND_TILE, (1, 1): LAND_TILE}
+        test_character = {'x-coordinate': 0, 'y-coordinate': 0}
+        test_direction = False
+        expected = (0, 0), False
+        actual = validate_move(test_board, test_character, test_direction, 2, 2)
+        self.assertEqual(expected, actual)
+
+    def test_fishing_false(self):
+        test_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): LAND_TILE, (1, 1): LAND_TILE}
+        test_character = {'x-coordinate': 0, 'y-coordinate': 0, 'rod level': 0}
+        test_direction = 'fishing'
+        expected = 'fishing', False
+        actual = validate_move(test_board, test_character, test_direction, 2, 2)
+        self.assertEqual(expected, actual)
+
+    def test_fishing_true(self):
+        test_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): LAND_TILE, (1, 1): WATER_TILE}
+        test_character = {'x-coordinate': 1, 'y-coordinate': 0, 'rod level': 0}
+        test_direction = 'fishing'
+        expected = 'fishing', True
+        actual = validate_move(test_board, test_character, test_direction, 2, 2)
+        self.assertEqual(expected, actual)
+
+    def test_x_direction_false(self):
+        test_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): LAND_TILE, (1, 1): LAND_TILE}
+        test_character = {'x-coordinate': 0, 'y-coordinate': 0}
+        test_direction = (0, -1)
+        expected = (0, 0), False
+        actual = validate_move(test_board, test_character, test_direction, 2, 2)
+        self.assertEqual(expected, actual)
+
+    def test_x_direction_true(self):
+        test_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): LAND_TILE, (1, 1): LAND_TILE}
+        test_character = {'x-coordinate': 0, 'y-coordinate': 0}
+        test_direction = (0, 1)
+        expected = (0, 1), True
+        actual = validate_move(test_board, test_character, test_direction, 2, 2)
+        self.assertEqual(expected, actual)
+
+    def test_y_direction_false(self):
+        test_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): LAND_TILE, (1, 1): LAND_TILE}
+        test_character = {'x-coordinate': 1, 'y-coordinate': 1}
+        test_direction = (2, 1)
+        expected = (1, 1), False
+        actual = validate_move(test_board, test_character, test_direction, 2, 2)
+        self.assertEqual(expected, actual)
+
+    def test_y_direction_true(self):
+        test_board = {(0, 0): LAND_TILE, (0, 1): LAND_TILE, (1, 0): LAND_TILE, (1, 1): LAND_TILE}
+        test_character = {'x-coordinate': 1, 'y-coordinate': 1}
+        test_direction = (0, 1)
+        expected = (0, 1), True
+        actual = validate_move(test_board, test_character, test_direction, 2, 2)
+        self.assertEqual(expected, actual)
+
+    def test_move_onto_water_false(self):
+        test_board = {(0, 0): LAND_TILE, (0, 1): WATER_TILE, (1, 0): LAND_TILE, (1, 1): LAND_TILE}
+        test_character = {'x-coordinate': 0, 'y-coordinate': 0, 'rod level': 0}
+        test_direction = (0, 1)
+        expected = (0, 0), False
+        actual = validate_move(test_board, test_character, test_direction, 2, 2)
+        self.assertEqual(expected, actual)
+
+    def test_move_onto_water_true(self):
+        test_board = {(0, 0): LAND_TILE, (0, 1): WATER_TILE, (1, 0): LAND_TILE, (1, 1): LAND_TILE}
+        test_character = {'x-coordinate': 0, 'y-coordinate': 0, 'rod level': 1}
+        test_direction = (0, 1)
+        expected = (0, 1), True
+        actual = validate_move(test_board, test_character, test_direction, 2, 2)
+        self.assertEqual(expected, actual)
